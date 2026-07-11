b01413fc2c80b3fdc65bb0635ba2d8d67833abbc
Arun Et-008 <arunet008@gmail.com>
2026-07-11 15:01:51 +0530

    [Fix] TOC updated.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -37,6 +37,11 @@ export default function About() {
       items: [],
     },
     {
+      title: about.technical.title,
+      display: about.technical.display,
+      items: about.technical.skills.map((skill) => skill.title),
+    },
+    {
       title: about.work.title,
       display: about.work.display,
       items: about.work.experiences.map((experience) => experience.company),
@@ -45,11 +50,6 @@ export default function About() {
       title: about.studies.title,
       display: about.studies.display,
       items: about.studies.institutions.map((institution) => institution.name),
-    },
-    {
-      title: about.technical.title,
-      display: about.technical.display,
-      items: about.technical.skills.map((skill) => skill.title),
     },
   ];
   return (
